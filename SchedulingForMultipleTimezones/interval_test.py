2154ee4a47161eb65fa0c964cef66df26016bc2a
Tianle Chen <tianlechen@gmail.com>
2020-10-25 23:15:44 -0400

    test for unions

--- SchedulingForMultipleTimezones/interval_test.py
+++ SchedulingForMultipleTimezones/interval_test.py
@@ -9,16 +9,19 @@ left = Interval(0, 2)
 # [-r-]
 a_right = Interval(0, 1)
 a_intersect = a_right
+a_union = left
 # Case B
 # [--l--]
 #   [-r-]
 b_right = Interval(1, 2)
 b_intersect = b_right
+b_union = left
 # Case C
 # [--l--]
 #   [--r--]
 c_right = Interval(1, 3)
 c_intersect = Interval(1, 2)
+c_union = Interval(0, 3)
 
 
 def test_negative_range_error():
@@ -38,6 +41,16 @@ def test_equality(interval):
     pytest.param(left, b_right, b_intersect, id='Case B'),
     pytest.param(left, c_right, c_intersect, id='Case C'),
 ])
-def test_rand(interval0, interval1, intervalexpected):
+def test_and(interval0, interval1, intervalexpected):
     assert interval0 & interval1 == intervalexpected
     assert interval1 & interval0 == intervalexpected
+
+
+@pytest.mark.parametrize("interval0, interval1, intervalexpected", [
+    pytest.param(left, a_right, a_union, id='Case A'),
+    pytest.param(left, b_right, b_union, id='Case B'),
+    pytest.param(left, c_right, c_union, id='Case C'),
+])
+def test_or(interval0, interval1, intervalexpected):
+    assert interval0 | interval1 == intervalexpected
+    assert interval1 | interval0 == intervalexpected
