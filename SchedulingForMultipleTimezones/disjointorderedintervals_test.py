2573702c6c42a0c2effa4d919452f4a5fb66dae4
Tianle Chen <tianlechen@gmail.com>
2020-10-26 00:03:35 -0400

    test_is_disjoint_ordered, test_get_disjoint_ordered_intervals

--- SchedulingForMultipleTimezones/disjointorderedintervals_test.py
+++ SchedulingForMultipleTimezones/disjointorderedintervals_test.py
@@ -1,9 +1,31 @@
 import pytest
 
-from SchedulingForMultipleTimezones.disjointorderedintervals import DisjointOrderedIntervals, \
-    get_left_interval_minus_right
+from SchedulingForMultipleTimezones.disjointorderedintervals import (
+    DisjointOrderedIntervals, get_disjoint_ordered_intervals,
+    get_left_interval_minus_right, is_disjoint_ordered)
 from SchedulingForMultipleTimezones.interval import Interval
 
+
+@pytest.mark.parametrize("intervals, expected", [
+    pytest.param(
+        [Interval(0, 1), Interval(2, 3)], True,
+        id='2 disjoint ordered intervals'
+    ),
+    pytest.param(
+        [Interval(2, 3), Interval(0, 1)], False,
+        id='2 disjoint non-ordered intervals'
+    ),
+    pytest.param(
+        [Interval(0, 1), Interval(1, 2)], False,
+        id='2 non-disjoint ordered intervals'
+    ),
+    pytest.param(
+        [Interval(1, 2), Interval(0, 1)], False,
+        id='2 non-disjoint non-ordered intervals'
+    ),
+])
+def test_is_disjoint_ordered(intervals, expected):
+    assert is_disjoint_ordered(intervals) == expected
 
 @pytest.mark.parametrize("left, right, expected", [
     pytest.param(
@@ -30,7 +52,7 @@ from SchedulingForMultipleTimezones.interval import Interval
 def test_get_left_interval_minus_right(left, right, expected):
     assert get_left_interval_minus_right(left, right) == expected
 
-# def test_get_disjoint_ordered_intervals():
-#     intervals = [Interval(0, 2), Interval(1, 3)]
-#     expected = [Interval(0, 3)]
-#     assert get_disjoint_ordered_intervals(intervals) == expected
+def test_get_disjoint_ordered_intervals():
+    intervals = [Interval(0, 2), Interval(1, 3)]
+    expected = DisjointOrderedIntervals([Interval(0, 3)])
+    assert get_disjoint_ordered_intervals(intervals) == expected
